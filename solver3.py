fa3994b9715dc8de027d263cdad6bd2907423a0d
Даниил Копылов <it-daniil@yandex.ru>
2023-08-27 08:11:48 +0800

    левая, правая граница и центральные узлы

--- solver3.py
+++ solver3.py
@@ -17,6 +17,7 @@ T0 = 0
 x_an = lambda s, t: np.exp(s)*np.cos(t)
 y_an = lambda s, t: (s+2)*np.sin(t)
 
+
 class Node:
     def __init__(self, s, t, left=None, right=None):
         self.s = s
@@ -83,6 +84,14 @@ class NodeStart(Node):
             self.y = y0(self.s)
 
 
+class NodeLeft(Node):
+    def solver_s0(self):
+        pass
+
+class NodeRight(Node):
+    def solver_s1(self):
+        pass
+
 class Mesh:
     def __init__(self):
         self.c1 = C1
@@ -91,12 +100,12 @@ class Mesh:
         self.t1 = 4
         self.s0 = 0
         self.s1 = 2
-        self.m = 30
+        self.m = 12
         self.ds, self.h_down, self.h_up = self.create_mesh_s()
         self.start = MeshStart(self.ds, [self.h_down, self.h_up], self.m, self.s0, self.t0)
-        self.center = MeshCenter()
-        self.left = MeshLeft()
-        self.right = MeshRight()
+        self.center = MeshCenter(self.ds, [self.h_down, self.h_up], self.m)
+        self.left = MeshLeft([self.h_down, self.h_up], self.s0, self.t1)
+        self.right = MeshRight([self.h_down, self.h_up], self.s1, self.t1)
 
     def create_mesh_s(self):
         ds = (self.s1 - self.s0) / self.m
@@ -105,24 +114,34 @@ class Mesh:
         return ds, h_down, h_up
 
     def plot(self):
-        nodes = self.start.get_nodes()
-        for node in nodes:
+        start_nodes = self.start.get_nodes()
+        for node in start_nodes: # начальные условия
             l, r = node.get_parents_node([self.c1, self.c2], self.t0, [self.h_down, self.h_up])
             l.solver_t0(self.t0)
             r.solver_t0(self.t0)
             node.solver_t0(self.t0)
 
-        for node in nodes:
+        for node in start_nodes: # решение гип сис первых узлов
             if node.t != self.t0:
-               node.solver_node()
-            print(node)
+                node.solver_node()
 
-        for node in nodes:
+        left_nodes = self.left.get_nodes(start_nodes[0])
+        right_nodes = self.right.get_nodes(start_nodes[-1])
+        center_nodes = self.center.get_nodes(start_nodes[1:-1], left_nodes, right_nodes)
+        for node in start_nodes:
             plt.scatter(node.s, node.t, color="g")
             l, r = node.get_parents_node([self.c1, self.c2], self.t0, [self.h_down, self.h_up])
             plt.scatter(l.s, l.t, color="r", marker=".")
             plt.scatter(r.s, r.t, color="r", marker=".")
 
+        for node in left_nodes:
+            plt.scatter(node.s, node.t, color="b")
+        for node in right_nodes:
+            plt.scatter(node.s, node.t, color="b")
+
+        for level in center_nodes:
+            for node in level:
+                plt.scatter(node.s, node.t, color="gray")
         plt.xlim([self.s0, self.s1])
         plt.ylim([self.t0, self.t1])
         plt.show()
@@ -155,15 +174,60 @@ class MeshStart:
 
 
 class MeshCenter:
-    pass
+    def __init__(self, ds, h, m):
+        self.ds = ds
+        self.dt = h[0]+h[1]
+        self.h = h
+        self.m = m
+
+    def get_nodes(self, start_nodes, left_nodes, right_nodes):
+        count_level = len(left_nodes)
+        nodes = [[Node(node.s, node.t+self.dt*i) for node in start_nodes] for i in range(1, count_level)]
+        for i, left_node in enumerate(left_nodes[1:]):
+            nodes[i][0].left = left_node
+        start_right = 1 if start_nodes[-1].t > right_nodes[0].t else 0
+        for i, right_node in enumerate(right_nodes[start_right:]):
+            nodes[i][-1].right = right_node
+        return nodes
 
 
 class MeshLeft:
-    pass
+    def __init__(self, h, s0, t1):
+        self.h = h
+        self.s0 = s0
+        self.t1 = t1
+
+    def get_nodes(self, start_node):
+        dt = self.h[0]+self.h[1]
+        t = np.arange(start_node.t+dt, self.t1, dt)
+
+        start_left_node = NodeLeft(start_node.s, start_node.t, right=start_node.right)
+        start_left_node.x = start_node.x
+        start_left_node.y = start_node.y
+
+        nodes = [start_left_node]
+
+        nodes = nodes + [NodeLeft(self.s0, ti) for ti in t]
+        return nodes
 
 
 class MeshRight:
-    pass
+    def __init__(self, h, s1, t1):
+        self.h = h
+        self.s1 = s1
+        self.t1 = t1
+
+    def get_nodes(self, start_node):
+        dt = self.h[0] + self.h[1]
+        t = np.arange(start_node.t+dt, self.t1, dt)
+
+        start_right_node = NodeRight(start_node.s, start_node.t, left=start_node.left)
+        start_right_node.x = start_node.x
+        start_right_node.y = start_node.y
+
+        nodes = [start_right_node]
+        nodes = nodes + [NodeRight(self.s1, ti) for ti in t]
+        return nodes
 
 
 if __name__ == '__main__':
